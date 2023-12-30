6ff6caf66f16e940977a3b066d506a10ae35141b
tanvir <tanvir0046@gmail.com>
2023-12-30 14:46:57 +0600

    update work done

--- index.js
+++ index.js
@@ -157,20 +157,25 @@ async function run() {
             const result = await menuCollection.findOne(query)
             res.send(result);
         })
-        // app.get('/menu/:id', async (req, res) => {
-        //     try {
-        //         const id = req.params.id;
-        //         const query = { _id: new ObjectId(id) }
-        //         const result = await menuCollection.findOne(query)
-        //         res.send(result);
-        //     } catch (error) {
-        //         console.error(error);
-        //         res.status(500).send('Internal Server Error');
-        //     }
-        // });
         app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
             const item = req.body;
             const result = await menuCollection.insertOne(item);
+            res.send(result)
+        })
+        app.patch('/menu/:id', async (req, res) => {
+            const item = req.body;
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    name: item.name,
+                    category: item.category,
+                    price: item.price,
+                    recipe: item.recipe,
+                    image: item.image
+                }
+            }
+            const result = await menuCollection.updateOne(filter, updateDoc);
             res.send(result)
         })
         app.delete('/menu/:id', async (req, res) => {
