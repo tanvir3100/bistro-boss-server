a58bdb998e0cd96f0730bb95d3ca5660e7b02a49
tanvir <tanvir0046@gmail.com>
2024-08-27 00:20:50 +0600

    all work is done

--- index.js
+++ index.js
@@ -240,16 +240,3 @@ app.listen(port, () => {
 
 
 
-
-/**
- * ------------------------------------------
- *             NAMING CONVENTION
- * ------------------------------------------
- * 
- * app.get('/users')
- * app.get('/users/:id')
- * app.post('/users')
- * app.put('/users/:id')
- * app.patch('/users/:id')
- * app.delete('/users/:id')
-*/
